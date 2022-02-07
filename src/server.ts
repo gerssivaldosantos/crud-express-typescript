cdf9ed49cabf75866342d6ff20c1373da937797a
gerssivaldosantos <canalandows@gmail.com>
2022-02-07 04:17:42 -0300

    importing the routes

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,12 @@ import express from "express";
 
 import "./database";
 
+import { routes } from "./services/routes";
+
 const app = express();
 
+app.use(express.json());
 
+app.use(routes)
 
 app.listen(8085, () => console.log("Server started"));
