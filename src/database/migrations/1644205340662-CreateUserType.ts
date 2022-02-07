153cd01b4bd06f097c6e1abc082ebf9c7e539f08
gerssivaldosantos <canalandows@gmail.com>
2022-02-07 00:48:28 -0300

    migration up

---
+++ src/database/migrations/1644205340662-CreateUserType.ts
@@ -0,0 +1,42 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateUserType1644205340662 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'user_type',
+                columns:[
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                        isNullable: false,
+                        isUnique: true,
+                    },
+                    {
+                        name: 'description',
+                        type: 'varchar',
+                        isNullable: true,
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    }
+                ]
+                
+
+
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
