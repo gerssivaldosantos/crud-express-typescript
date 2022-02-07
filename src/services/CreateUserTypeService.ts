2c74cbffd7f3bffdb07cfde4a899e2a93cc60ba7
gerssivaldosantos <canalandows@gmail.com>
2022-02-07 03:06:24 -0300

    service to user_type

---
+++ src/services/CreateUserTypeService.ts
@@ -0,0 +1,32 @@
+import { getRepository } from "typeorm";
+import { UserType } from "../entities/UserType";
+
+type UserTypeRequest = {
+    name: string;
+    description: string;
+}
+export class CreateCategoryService {
+    async execute({
+        name,
+        description
+    }: UserTypeRequest): Promise<UserType> {
+        const repo = getRepository(UserType);
+
+        if (
+            //SELECT * FROM user_type WHERE name = 'name' LIMIT 1
+            await repo.findOne({ name })
+        ) {
+            throw new Error('UserType already exists');
+        }
+        else {
+            const user_type = repo.create({
+                name,
+                description,
+            })
+
+            await repo.save(user_type);
+
+            return user_type;
+        }
+    }
+}
