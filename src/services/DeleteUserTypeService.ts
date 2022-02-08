b2b08691a6eb2176f022c2b856fcdb238b740777
gerssivaldosantos <canalandows@gmail.com>
2022-02-08 03:58:19 -0300

    new services to delete type user

---
+++ src/services/DeleteUserTypeService.ts
@@ -0,0 +1,24 @@
+import { getRepository } from 'typeorm';
+import { UserType } from '../entities/UserType';
+
+export class DeleteUserTypeService{
+    public async execute(id): Promise<{}> {
+        try{
+            const user_typeRepository = getRepository(UserType);
+            await user_typeRepository.delete({id: id});
+            return {
+                status: 200,
+                message: `User type id=${id} deleted`
+            }
+            
+        }
+        catch(error){
+            console.log(error)
+            return {
+                status: 404,
+                message: `User type id=${id} not found`
+            }
+        }
+
+    }
+}
--- src/services/routes.ts
+++ src/services/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { CreateUserTypeController } from "../controllers/CreateUserTypeController";
+import { DeleteUserTypeController } from "../controllers/DeleteUserTypeController";
 import { GetAllUserTypeController } from "../controllers/GetAllTypeUserController";
 
 const routes = Router();
 
 routes.post("/user_type", new CreateUserTypeController().handle);
+routes.delete("user_type/:id", new DeleteUserTypeController().handle);
 routes.get("/user_type", new GetAllUserTypeController().handle);
 
 export { routes }
