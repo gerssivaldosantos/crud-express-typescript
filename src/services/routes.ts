5b98b9d0f031945e6fd104fe5ef401bba076473c
gerssivaldosantos <canalandows@gmail.com>
2022-02-08 13:51:19 -0300

    new route to update user type

--- src/services/routes.ts
+++ src/services/routes.ts
@@ -3,10 +3,12 @@ import { CreateUserTypeController } from "../controllers/CreateUserTypeControlle
 import { DeleteUserTypeController } from "../controllers/DeleteUserTypeController";
 import { GetAllUserTypeController } from "../controllers/GetAllTypeUserController";
 import { GetUserTypeIdController } from "../controllers/GetUserTypeIdController";
+import { UpdateUserTypeController } from "../controllers/UpdateUserTypeController";
 
 const routes = Router();
 
 routes.post("/user_type", new CreateUserTypeController().handle);
+routes.put("/user_type/:id", new UpdateUserTypeController().handle);
 routes.delete("/user_type/:id", new DeleteUserTypeController().handle);
 routes.get("/user_type", new GetAllUserTypeController().handle);
 routes.get("/user_type_id/:name", new GetUserTypeIdController().handle);
