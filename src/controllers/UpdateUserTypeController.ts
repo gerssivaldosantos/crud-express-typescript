748a1d3a44f61dcb6bcb3f76ce1698a327554e86
gerssivaldosantos <canalandows@gmail.com>
2022-02-08 13:50:00 -0300

    update user type controller

---
+++ src/controllers/UpdateUserTypeController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { UserTypeUpdateRequest } from "../models/UserTypeModel";
+import { UpdateUserTypeService} from "../services/UpdateUserTypeService";
+
+export class UpdateUserTypeController{
+    async handle(request: Request, response: Response){
+        const user_type_update = <UserTypeUpdateRequest>{
+            id: request.params.id,
+            name: request.body.name,
+            description: request.body.description
+        }
+        const service = new UpdateUserTypeService();
+        const result = await service.execute(user_type_update);
+
+        return response.json(result);
+    }
+}
