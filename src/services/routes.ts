61d03901f46b9fd688537c7abe2331fe3f2e66b9
gerssivaldosantos <canalandows@gmail.com>
2022-02-09 10:55:10 -0300

    feat: new route for user creation

--- src/services/routes.ts
+++ src/services/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { CreateUserController } from "../controllers/CreateUserController";
 import { CreateUserTypeController } from "../controllers/CreateUserTypeController";
 import { DeleteUserTypeController } from "../controllers/DeleteUserTypeController";
 import { GetAllUserTypeController } from "../controllers/GetAllTypeUserController";
@@ -7,9 +8,15 @@ import { UpdateUserTypeController } from "../controllers/UpdateUserTypeControlle
 
 const routes = Router();
 
+/* Routes for user */
+
+routes.post("/user", new CreateUserController().handle);
+
+/* Routes for user type */
 routes.post("/user_type", new CreateUserTypeController().handle);
 routes.put("/user_type/:id", new UpdateUserTypeController().handle);
 routes.delete("/user_type/:id", new DeleteUserTypeController().handle);
 routes.get("/user_type", new GetAllUserTypeController().handle);
 routes.get("/user_type_id/:name", new GetUserTypeIdController().handle);
+
 export { routes }
