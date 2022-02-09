ad861885a0888fee9021cbe255f745bc0d0e5ad8
gerssivaldosantos <canalandows@gmail.com>
2022-02-09 14:32:27 -0300

    feat: new type for user update

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -4,3 +4,11 @@ export type UserRequest = {
     password: string;
     user_type_id: string;
 }
+
+export type UserUpdateRequest = {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    user_type_id: string;
+}
